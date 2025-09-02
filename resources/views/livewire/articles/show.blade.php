739d6962319b7db07b629a85f2494c20f889fa9b
toshi565 <ube.toshi.13@gmail.com>
2025-09-02 10:43:41 +0900

    課題１完了

--- resources/views/livewire/articles/show.blade.php
+++ resources/views/livewire/articles/show.blade.php
@@ -1,11 +1,20 @@
 <?php
 
 use function Livewire\Volt\{state};
+use App\Models\Article;
 
-//
+// ルートモデルバインディング
+state(['article' => fn(Article $article) => $article]);
+
+// 一覧ページに戻るメソッド
+$index = function () {
+    return redirect()->route('articles.index');
+};
 
 ?>
 
 <div>
-    //
+    <p>タイトル:{{ $article->title }}</p>
+    <p>{!! nl2br(e($article->body)) !!}</p>
+    <button wire:click="index">一覧へ戻る</button>
 </div>
